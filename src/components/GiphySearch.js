22768e813b51b99896866fdf7570ce7499503ff5
Todd Garrison <toddlgarrison@gmail.com>
2023-10-19 13:33:18 -0400

    Create Giphy search

--- src/components/GiphySearch.js
+++ src/components/GiphySearch.js
@@ -3,4 +3,38 @@ import React, { useState } from "react";
 const GiphySearch = () => {
     const [query, setQuery] = useState('')
     const [gifs, setGifs] = useState([])
+    
+    const giphyApiKey = process.env.GIPHY_API_KEY
+
+    const handleSearch = async () => {
+        try {
+            const response = await fetch(`https://api.giphy.com/v1/gifs/search?q=${query}&api_key=${giphyApiKey}`)
+            const giphyData = await response.json()
+
+            console.log(giphyData)
+            setGifs(giphyData.data)
+        } catch (error) {
+            console.error(`Error in fetch: ${error.message}`)
+        }
+    }
+
+    return (
+        <div>
+            <input 
+                type="text"
+                placeholder="Search for Gifs"
+                value={query}
+                onChange={(event) => setQuery(event.target.value)}
+            />
+            <button onClick={handleSearch}>Search</button>
+
+            <div>
+                {gifs.map((gif) => (
+                    <img key={gif.id} src={gif.images.fixed_height.url} alt={gif.title} />
+                ))}
+            </div>
+        </div>
+    )
 }
+
+export default GiphySearch
