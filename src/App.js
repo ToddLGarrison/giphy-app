5f059f466665b7f9a751044f2123f880e15b8ef1
Todd Garrison <toddlgarrison@gmail.com>
2023-10-19 15:04:09 -0400

    Test giphy search

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import React from 'react';
+import GiphySearch from './components/GiphySearch';
 
 function App() {
   return (
     <div>
       <h1>Hello there</h1>
+      <GiphySearch />
     </div>
   );
 }
--- src/components/GiphySearch.js
+++ src/components/GiphySearch.js
@@ -4,17 +4,23 @@ const GiphySearch = () => {
     const [query, setQuery] = useState('')
     const [gifs, setGifs] = useState([])
     
-    const giphyApiKey = process.env.GIPHY_API_KEY
+    const giphyApiKey = process.env.REACT_APP_GIPHY_API_KEY;
 
     const handleSearch = async () => {
         try {
-            const response = await fetch(`https://api.giphy.com/v1/gifs/search?q=${query}&api_key=${giphyApiKey}`)
+            const response = await fetch(`https://api.giphy.com/v1/gifs/search?q=${query}&api_key=${giphyApiKey}&limit=10`)
             const giphyData = await response.json()
 
             console.log(giphyData)
             setGifs(giphyData.data)
         } catch (error) {
             console.error(`Error in fetch: ${error.message}`)
+        }
+    }
+
+    const handleKeyPress = (event) => {
+        if (event.key === "Enter") {
+            handleSearch()
         }
     }
 
@@ -25,6 +31,7 @@ const GiphySearch = () => {
                 placeholder="Search for Gifs"
                 value={query}
                 onChange={(event) => setQuery(event.target.value)}
+                onKeyPress={handleKeyPress}
             />
             <button onClick={handleSearch}>Search</button>
 
